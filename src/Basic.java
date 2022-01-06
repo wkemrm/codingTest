0bb6c1b1c83ce0f909db8c7a56d13d037a51d9e2
DESKTOP-DJD8IRH\7yhn8 <wkemrm12@naver.com>
2022-01-06 18:15:59 +0900

    study 2022-01-06

--- src/Basic.java
+++ src/Basic.java
@@ -1,10 +1,26 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Stack;
 
-public class Main {
-    public static void main(String[] args) {
+public class Basic {
+    // 약수 구하기
+    public int problem1(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1 ; i <= n ; i++) {
+            if (n % i == 0){
+                list.add(i);
+            }
+        }
+
+        if (k - 1 >= list.size()) {
+            return 0;
+        }
+        return list.get(k - 1);
+    }
+
+    // 이진수
+    public void problem2(String[] args) {
         // 이진수
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
